c602f9487c699b286aad1023826d8954e82c3035
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2022-09-15 21:21:28 +0530

    Updated battery Configuration and new Dev tools

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
+# set fish_greeting ''÷
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
-alias ls='exa -alh'
+alias ls='lsd -la'
 alias config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
 alias docs="git --git-dir=$HOME/.sdysp --work-tree=$HOME"
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
