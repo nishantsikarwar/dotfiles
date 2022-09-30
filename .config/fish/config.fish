dc43abe81c6e9fbc0ef6c2fa32f3770a8a375c7c
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2022-09-30 10:48:39 +0530

    Fish Shell default with tweaks in  configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+colorscript random
 alias ls='lsd -la'
 alias config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
 alias docs="git --git-dir=$HOME/.sdysp --work-tree=$HOME"
