0d7152c5cb2453569364f6c9e019e9672358960d
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2022-11-14 11:09:58 +0530

    Configured Go PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,21 @@ alias ls='lsd -la'
 alias config="git --git-dir=$HOME/.cfg --work-tree=$HOME"
 alias docs="git --git-dir=$HOME/.sdysp --work-tree=$HOME"
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+set GOPATH $HOME/go
 enable_transience
+
+# set GNUBINS "$(find `brew --prefix`/opt -type d -follow -name gnubin -print)"
+
+# for bindir in ${GNUBINS[@]}
+# do
+#   export PATH=$bindir:$PATH
+# done
+# end
+# export PATH
+
+# set GNUBINS "$(find (brew --prefix)/opt -type d -follow -name gnubin -print)"
+# # export GNUBINS="$(find /usr/local/opt -type d -follow -name gnubin -print)";
+
+# for bindir in $GNUBINS; 
+#   set PATH $bindir:$PATH;
+# end;
