c5c45738c077045b30c008354cd36aa54a0d5304
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2024-11-14 18:06:45 +0530

    update: fish config
    Signed-off-by: nishantsikarwar <nsikarwar@ch.iitr.ac.in>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,34 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+
+
+set STARSHIP_CONFIG "$ZDOTDIR/starship/config.toml"
+set STARSHIP_CACHE "$ZDOTDIR/starship/cache"
 starship init fish | source
 colorscript --exec doom-outlined
 alias ls='lsd -la'
